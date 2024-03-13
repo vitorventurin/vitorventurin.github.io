995c1872a98fea4883e4ed28558d622f17f0dda0
odious-desire <odious.desire.band@gmail.com>
2024-03-13 23:23:03 +0100

    parallax alegria

--- js/scripts.js
+++ js/scripts.js
@@ -67,3 +67,30 @@
   
   })(jQuery); // End of use strict
   
+
+
+///////////////////////
+// Parallax
+  const parallax1 = document.getElementsByClassName('parallax1');
+new simpleParallax(parallax1, {
+    scale: 0.8,
+    delay: 0.5,
+});
+
+const parallax2 = document.getElementsByClassName('parallax2');
+new simpleParallax(parallax2, {
+    scale: 0.9,
+    delay: 1,
+});
+
+const parallaxR = document.getElementsByClassName('parallaxR');
+new simpleParallax(parallaxR, {
+    orientation: 'right',
+    scale: 0.8,
+});
+
+const parallaxL = document.getElementsByClassName('parallaxL');
+new simpleParallax(parallaxL, {
+    orientation: 'left',
+    scale: 0.8,
+});
